3a9a29c54a668d8fe5325dbef524243e484aefd3
Rishaubh Kumar <rishaubhkr@gmail.com>
2025-05-18 22:04:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Balsamiq_Sans } from "next/font/google";
+import { Balsamiq_Sans } from 'next/font/google';
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
