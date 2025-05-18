522596d4bb8629035007bdf4c813c2c597a3b7f9
Rishaubh Kumar <rishaubhkr@gmail.com>
2025-05-18 22:32:56 +0530

    Update page.tsx

--- app/quiz/result/page.tsx
+++ app/quiz/result/page.tsx
@@ -2,10 +2,12 @@
 import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import confetti from 'canvas-confetti'
-import { Check, Crosshair, SkipForward, X } from 'lucide-react'
+import { Check, Crosshair, RotateCcw, SkipForward, X } from 'lucide-react'
 import { QuizType, ResultType } from '@/lib/types'
+import { useRouter } from 'next/navigation'
 
 const Result = () => {
+    const router = useRouter()
     const [result, setResult] = useState<ResultType>()
     const [quiz, setQuiz] = useState<QuizType>()
 
@@ -23,7 +25,7 @@ const Result = () => {
     } else {
         return (
             <main className='flex justify-center w-screen h-full items-center bg-transparent p-5'>
-                <header className='bg-primary/5 flex items-center h-16 justify-between rounded-xl top-4 fixed sm:w-[800px] w-[calc(100%-32px)] px-8 border border-primary/10'>
+                <header className='bg-primary/5 z-50 flex items-center h-16 justify-between rounded-xl top-4 fixed sm:w-[800px] w-[calc(100%-32px)] px-8 border border-primary/10'>
                     <p className='text-primary font-bold text-2xl'>QuizComp</p>
                 </header>
 
@@ -32,7 +34,7 @@ const Result = () => {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.6 }}
                 >
-                    <div className='bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl sm:p-8 p-5 shadow-xl sm:w-[560px]'>
+                    <div className='bg-white/5 mt-8 backdrop-blur-md border border-white/10 rounded-2xl sm:p-8 p-5 shadow-xl sm:w-[560px]'>
                         <div className='p-5 pt-5 space-y-4'>
                             <div className='space-y-1'>
                                 <p className='text-2xl font-bold flex items-center gap-2'>Result Summary</p>
@@ -103,8 +105,17 @@ const Result = () => {
                                             : '0%'
                                     }</p>
                                 </motion.div>
-
                             </div>
+
+                            <motion.button
+                                onClick={() => router.push("/quiz/question/0")}
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.95 }}
+                                className="bg-primary hover:bg-primary/90 flex items-center gap-2 justify-center cursor-pointer w-full text-black font-bold h-14 rounded-xl text-lg shadow-lg"
+                            >
+                                <p>Retry</p>
+                                <RotateCcw className='size-4' />
+                            </motion.button>
                         </div>
                     </div>
 
